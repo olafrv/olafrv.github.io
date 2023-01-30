fdcb65f947749e26c5739baf8f78933119895c6e
olafrv <olafrv@gmail.com>
2023-01-30 20:39:32 +0100

    GitHub Pages

---
+++ src/plugins/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/views/HomePage.vue"),
+    }
+  ],
+});
+
+export default router;
+
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,19 @@
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi"; // Not optimal for production
+import { md2 } from "vuetify/blueprints";
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+  blueprint: md2,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
